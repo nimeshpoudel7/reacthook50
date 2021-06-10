b091776eb45ca544c34a1d32cfd6be58b2606faa
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-06-10 12:03:00 +0545

    UseContext

--- src/components/ComponentB.js
+++ src/components/ComponentB.js
@@ -1,23 +1,14 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import ComponentC from './ComponentC'
 import {hey,colorContext} from '../App'
-
 const ComponentB = () => {
+    const Name = useContext(hey)
+    const Color = useContext(colorContext)
     return (
         <div>
             <div>I am B</div>
-           
-            <hey.Consumer>
-                {name=>{
-                    return( <colorContext.Consumer>
-                        {color=>(
-                             <div>
-                                name:{name} color:{color}
-                            </div>
-                        )}
-                    </colorContext.Consumer>)
-                }}
-            </hey.Consumer>
+            <div>Name from B: {Name}</div>
+            <div>Color from B: {Color}</div>
             <ComponentC/>
         </div>
     )
--- src/components/ComponentC.js
+++ src/components/ComponentC.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import {hey,colorContext} from '../App'
 const ComponentC = () => {
     return (
         <div>
             <div>i am c</div>
+            
+            <hey.Consumer>
+                {name=>{
+                    return( <colorContext.Consumer>
+                        {color=>(
+                             <div>
+                                name:{name} color:{color}
+                            </div>
+                        )}
+                    </colorContext.Consumer>)
+                }}
+            </hey.Consumer>
            
         </div>
     )
