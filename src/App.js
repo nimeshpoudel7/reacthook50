8d9fc1db969e34d605b467c633ae7763255a38ae
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-06-12 16:59:03 +0545

    axios useeffect for the use reduce

--- src/App.js
+++ src/App.js
@@ -1,41 +1,77 @@
-import React,{useReducer} from 'react';
+import React,{useEffect,useReducer} from 'react';
 import './App.css';
-// import Counter from './components/Counter'
-import ComponentA from './components/ComponentA'
-export const CounterContex = React.createContext()
-const initialState={
-  counter:0
-}
-const reducer = (state, action) => {
+import axios from 'axios'
+const initialState = {
+  loading: true,
+  error: "",
+  todos: [],
+};
+const reducer=(state,action)=>{
   switch (action.type) {
-      case 'increment':
-          console.log("stare: "+state.counter)
-          // console.log(action)
-          return {...state,counter:state.counter+action.payload}
-          
-      case 'decrement':
-        console.log("stare: "+state.counter)
-          return {...state,counter:state.counter-action.payload}
-      case 'reset':
-        console.log("stare: "+state.counter)
-          return initialState
-
-      default:
-          return state;
+    case 'SET_DATA':
+      return{
+        loading:false,
+        error:"",
+        todos:action.payload
+      }
+    case 'SET_ERROR':
+      
+      return{
+        loading:false,
+        error:'some errors',
+        todos:[]
+      }
+    
+    default:
+      return state
   }
 }
+
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users/1/todos')
+    // axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then (responseJson => {
+      console.log(responseJson.data)
+      dispatch({type:'SET_DATA',payload:responseJson.data})
+      // setobjUserDetails(responseJson.data)
+      // setobjUserDetail(responseJson.data)
+    })
+    .catch(responseJson => {
+      dispatch({type:'SET_ERROR'})
+    })
+  },[])
+ 
+
+  // let listmarkup=<div>sucess</div>
+  console.log(state.loading)
+  console.log(state.todos)
+   const CHECKDATA = (
+     <ul>
+       {state.todos.map((todo)=>{
+         return <li key={todo.id}>{todo.title}</li>
+       }
+       
+       )
+       }
+     </ul>
+   )
+  //  const listmarkup = (
+  //   <ul>
+  //     {state.todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+  //   </ul>
+  // )
+
+
   return (
-    <CounterContex.Provider value={{counter:state.counter,dispatch:dispatch}}>
+    
     <div className="App">
-     
-      App js counter {state.counter}
-      <ComponentA/>
-  
+ {state.loading ? 'Loading' : (state.error ? state.error : CHECKDATA)}
+
     </div>
-    </CounterContex.Provider>
   );
-}
+}     
 
 export default App;
