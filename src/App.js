73f9e483b3a81498b24ee6066523efd9f63c8677
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-06-15 14:50:15 +0545

    use callback

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React,{useRef,useState} from 'react';
+import React,{useCallback,useState} from 'react';
 import './App.css';
 import { Button  } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import UseRefSetTime from './components/UseRefSetTime';
 
 
 function App() {
-  const [show, setShow] = useState(true)
- const inputRef = useRef()
+  const functioncountr= new Set()
+  const [Counter1, setCounter1] = useState(0)
+  const [Counter2, setCounter2] = useState(0)
+
+  const increment1 =()=>{
+    console.log(increment1)
+    setCounter1(Counter1+1)
+  }
+
+  const increment2=useCallback(
+    () => {
+      setCounter2(Counter2+1)
+    },
+    [Counter2],
+  )
+  functioncountr.add(Counter1)
+  functioncountr.add(Counter2)
+  console.log(functioncountr)
   return (
     
     <div className="App">
-    <input ref={inputRef}type="text"></input>
-    <input type="text"></input>
-    <input type="text"></input>
-    <Button onClick={()=>{
-      inputRef.current.focus()
-    }}> Use OF ref</Button>
+    <h1>Counter 1 value: {Counter1}</h1>
+    <h1>Counter 1 value: {Counter2}</h1>
+    <Button onClick={increment1}>Counter 1 Btn</Button>
     <p></p>
-    <Button onClick={()=>{setShow(!show)}}>hide and show</Button>
-
-    {show && <UseRefSetTime/>}
+    <Button onClick={increment2}>Counter 2 btn</Button>
     </div>
     
   );
