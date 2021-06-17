ba41dbd3650b01b56029301b8e702a9a56ff3576
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-06-17 14:54:09 +0545

    hook finished

--- src/Customhook/useInput.js
+++ src/Customhook/useInput.js
@@ -1,16 +1,16 @@
 import { useState } from "react"
 
-const useInput = (initialState) => {
+const useInput = (initialState='') => {
   const [value, setValue] = useState(initialState)
-  const cleartexr=()=>{
+  const cleartext=()=>{
     setValue(initialState)
   }
   const bindForm={
         value: value,
-        onChange: event=>{setValue(event.target.value)
-        }
+        onChange: event=>{setValue(event.target.value)},
+        type:'email'
   }
-  return [value,bindForm,cleartexr]
+  return [value,bindForm,cleartext]
 }
 export default useInput
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,25 @@
-import React,{useState} from 'react'
+import React from 'react'
+import useInput from '../Customhook/useInput'
 
 const Form = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const nim=(event)=>{
-        setName(event.target.value)
-    }
+    const [name, bindName, clearName] = useInput()
+    const [email, bindEmail, clearEmail] =useInput()
+
     const submitHandler=(event)=>{
         event.preventDefault()
         alert(`Hello ${name} is your email ${email}`)
-        setName('')
-        setEmail('')
+        clearName()
+        clearEmail()
     }
     return (
         <div>
             <form onSubmit={submitHandler}>
                 <div><label>Name</label>
-                    <input value={name} onChange={nim}></input>
+                    <input {...bindName}></input>
                     
                 </div>
                 <div><label>Email</label>
-                    <input value={email} onChange={event=>setEmail(event.target.value)}></input>
+                    <input {...bindEmail}></input>
                 </div>
                 <div>
                     <button> Submit</button>
