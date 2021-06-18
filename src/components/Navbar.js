09372c886ff9e225f12525319e4d9a3f8190d1e9
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-06-18 15:15:20 +0545

    navLik and link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react'
-
+import { Link,NavLink } from "react-router-dom";
 const Navbar = () => {
     return (
-        <nav className="nav nav-pills flex-column flex-sm-row">
-        <a className="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="/">Home</a>
-        <a className="flex-sm-fill text-sm-center nav-link" href="/about">ab</a>
-        <a className="flex-sm-fill text-sm-center nav-link" href="/contact">Contact</a>
-        
-      </nav>
-
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" href="#">Navbar</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="nav nav-tabs">
+              <NavLink className="nav-item nav-link" exact to="/">Home</NavLink>
+              <NavLink className="nav-item nav-link" to="/about">About</NavLink>
+              <NavLink className="nav-item nav-link" to="/contact">Contact</NavLink>
+          </div>
+      </div>
+  </nav>
 
 
     )
